bafc0bf39f9a14e53ab721452714f6e45515c846
SWS <wenshuaisun@outlook.com>
2017-09-28 17:54:44 +0800

    shfo

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Providers;
+
+use App\Article;
+use App\Discussion;
+use App\Tools\FileManager\BaseManager;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+use App\Tools\FileManager\UpyunManager;
+use Illuminate\Database\Eloquent\Relations\Relation;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $lang = config('app.locale') != 'zh_cn' ? config('app.locale') : 'zh';
+        \Carbon\Carbon::setLocale($lang);
+
+        Relation::morphMap([
+            'discussions' => Discussion::class,
+            'articles'    => Article::class,
+        ]);
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('uploader', function ($app) {
+            $config = config('filesystems.default', 'public');
+
+            if ($config == 'upyun') {
+                return new UpyunManager();
+            }
+
+            return new BaseManager();
+        });
+    }
+}
---
+++ resources/assets/js/components/dashboard/particals/FooterBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer>
+        <div class="copy-right text-center">
+            <span>© Wenshuai Sun 2017. All rights reserved. </span><a class="item" href="http://www.miitbeian.gov.cn/">粤ICP备16020344号-1</a>
+        </div>
+    </footer>
+</template>
